b2309e32100413e2a7a7fafb32562e6da4900a3a
Yudong Li <a1923172548@gmail.com>
2025-03-20 10:02:47 -0700

    Added vehicle movement speed
    Created movement speed for vehicle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
-
+    public float turnSpeed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
